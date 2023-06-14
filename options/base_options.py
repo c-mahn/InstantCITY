df9fc2efb7b6bf97d96aaff86bedceba34bb921f
cmahn <mail@cmahn.de>
2023-06-14 13:55:10 +0200

    Update base_options.py

--- options/base_options.py
+++ options/base_options.py
@@ -77,7 +77,8 @@ class BaseOptions():
         
         # set gpu ids
         if len(self.opt.gpu_ids) > 0:
-            torch.cuda.set_device(self.opt.gpu_ids[0])
+            print("hi")
+            torch.cuda.device(self.opt.gpu_ids[0])
 
         args = vars(self.opt)
 
